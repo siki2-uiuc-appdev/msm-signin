8f44595ed50a94784336fd1484a4084fcaac4d02
siki2-uiuc <siki2@illinois.edu>
2022-10-24 19:36:48 +0000

    Generated movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_10_24_193006) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.integer "duration"
+    t.integer "director_id"
+    t.text "description"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
